98c37c67ad2dd92e369cd5184b9a8d72f65fa969
Tim Golden <mail@timgolden.me.uk>
2018-02-26 14:31:47 +0000

    Try to set various env vars before building. Currently errors out in the middle of win32job

---
+++ build.cmd
@@ -0,0 +1,5 @@
+CALL C:\Users\goldent\.venvs\pywin32\Scripts\activate.bat
+SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64;C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x64
+SET MSSDK=C:\Program Files (x86)\Windows Kits\10
+PATH
+python setup3.py install
